4f179409090b45b1580fc66864f7122ad61bd2d6
Mayur Suryavanshi <suryavanshimayur4212@gmail.com>
2024-02-17 19:43:47 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const booksRoute = require("./routes/booksRoute");
+require("./connections/db");
+const cors = require("cors");
+
+//middleware.
+app.use("/api/v1",booksRoute);
+app.use(cors());
+//server creation
+app.listen(3000,()=>{
+    console.log("server running successfully.....");
+});
+
+app.get("/api/v1",(req,res)=>{
+    res.send("showing response");
+})
+
